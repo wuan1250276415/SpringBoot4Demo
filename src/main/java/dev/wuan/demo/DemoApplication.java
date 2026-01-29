a91afe64e9b587adfcba3094940b48d04ce660a2
wuan1 <wuan1250276415@gmail.com>
2026-01-29 17:34:49 +0800

    Remove test bomb logic from main application

--- src/main/java/dev/wuan/demo/DemoApplication.java
+++ src/main/java/dev/wuan/demo/DemoApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// 👇 加上这一行 "炸弹"
-		if (true) {
-			throw new RuntimeException("💥 这是一个用于测试回滚的人工炸弹！");
-		}
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
