f1024c831190f4cfca776512b786f95f2c788f86
wuan1 <wuan1250276415@gmail.com>
2026-01-29 17:28:33 +0800

    Add rollback logic and test bomb to deployment script

--- src/main/java/dev/wuan/demo/DemoApplication.java
+++ src/main/java/dev/wuan/demo/DemoApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// 👇 加上这一行 "炸弹"
+		if (true) {
+			throw new RuntimeException("💥 这是一个用于测试回滚的人工炸弹！");
+		}
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
